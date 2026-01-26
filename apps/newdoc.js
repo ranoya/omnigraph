c26cbd7f331c978f38b6e6c7c222cd912dbf1ab2
Guilherme Ranoya <ranoya@gmail.com>
2026-01-26 18:40:29 -0300

    criação de documentos

---
+++ apps/newdoc.js
@@ -0,0 +1,31 @@
+newdoc = function (u, c) {
+  let id = windowmanager.createdoc();
+
+  let code = `
+      <style>
+        #res${id} {
+
+            max-height: unset !important;
+            overflow-y: auto;
+            height: 100% 
+
+            margin: 0 !important;
+
+            width: calc(100% - 10px) !important;
+            max-height: unset !important;
+            height: 100%;
+            height: calc(100% - 5rem + 3px) !important;
+            padding: 5px !important;
+            padding-top: 7px !important;
+        
+        }
+      </style>
+      <iframe frameborder='0' style='width: calc(100%);height: 100%;height: calc(100% - 5px);' src='${u}'></iframe>
+      `;
+
+  document.getElementById("res" + id).innerHTML = code;
+
+  if (typeof c != "undefined" && c != null && c != "") {
+    document.getElementById("res" + id).style.backgroundColor = c;
+  }
+};
