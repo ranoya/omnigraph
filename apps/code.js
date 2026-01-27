c0db22b52481dac92ed63a936f5c6937d6d91742
Guilherme Ranoya <ranoya@gmail.com>
2026-01-27 16:31:03 -0300

    isso precisa funcionar

--- apps/code.js
+++ apps/code.js
@@ -3,72 +3,71 @@ code = function (p) {
 
   let code = `
     
-      <style>
-      
-      @import url("https://fonts.googleapis.com/css2?family=Fira+Code&display=swap");
+        <style>
+        @import url("https://fonts.googleapis.com/css2?family=Fira+Code&display=swap");
 
-      #editor_${id} {
-        width: 100%;
-        height: 100%;
-      }
+        #editor_${id} {
+            width: 100%;
+            height: 100%;
+        }
+        </style>
 
-      </style>
-    
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.15.2/ace.js"
-      type="text/javascript"></script>
-    
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.15.2/ext-language_tools.min.js"
-      type="text/javascript"></script>
+        <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.15.2/ace.js"
+        type="text/javascript"
+        ></script>
 
-      <script>
-    
-      let acegeneralconfig_e${id} = {
-        theme: "ace/theme/solarized_light",
-        mode: "ace/mode/html",
-        enableBasicAutocompletion: true,
-        enableSnippets: true,
-        enableLiveAutocompletion: true,
-        showPrintMargin: false,
-        highlightSelectedWord: true,
-        selectionStyle: "text",
-        highlightActiveLine: false,
-        cursorStyle: "wide",
-        wrapBehavioursEnabled: true,
-        wrap: true,
-        fontSize: "14px",
-        fontFamily: "Fira Code",
-        fadeFoldWidgets: true,
-      };
-    
-      let enviacomando_e${id} = function (instr) {
-        let aceed = ace.edit("editor_e${id}");
-        aceed.setKeyboardHandler("ace/keyboard/vscode");
-        aceed.setOptions(instr);
-      };
+        <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.15.2/ext-language_tools.min.js"
+        type="text/javascript"
+        ></script>
 
-      let lingua_e${id} = function (qual) {
-        let aceed = ace.edit("editor_e${id}");
-        aceed.setKeyboardHandler("ace/keyboard/vscode");
-        aceed.setOptions({ mode: "ace/mode/" + qual });
-      }
+        <script>
+        let acegeneralconfig_${id} = {
+            theme: "ace/theme/solarized_light",
+            mode: "ace/mode/html",
+            enableBasicAutocompletion: true,
+            enableSnippets: true,
+            enableLiveAutocompletion: true,
+            showPrintMargin: false,
+            highlightSelectedWord: true,
+            selectionStyle: "text",
+            highlightActiveLine: false,
+            cursorStyle: "wide",
+            wrapBehavioursEnabled: true,
+            wrap: true,
+            fontSize: "14px",
+            fontFamily: "Fira Code",
+            fadeFoldWidgets: true,
+        };
 
+        let enviacomando_${id} = function (instr) {
+            let aceed = ace.edit("editor_${id}");
+            aceed.setKeyboardHandler("ace/keyboard/vscode");
+            aceed.setOptions(instr);
+        };
 
-      </script>
+        let lingua_${id} = function (qual) {
+            let aceed = ace.edit("editor_${id}");
+            aceed.setKeyboardHandler("ace/keyboard/vscode");
+            aceed.setOptions({ mode: "ace/mode/" + qual });
+        };
+        </script>
 
-      <pre
-      class="editor codefull"
-      data-name="editor_e${id}"
-      id="editor_e${id}"
-      data-linguagem="html"></pre>
+        <pre
+        class="editor codefull"
+        data-name="editor_${id}"
+        id="editor_${id}"
+        data-linguagem="html"
+        ></pre>
 
-      <script>
+        <script>
+        let aceed_${id} = ace.edit("editor_${id}");
+        aceed_${id}.setTheme("ace/theme/monokai");
+        aceed_${id}.session.setMode("ace/mode/html");
+        aceed_${id}.setOptions(acegeneralconfig_${id});
+        </script>
 
-      let aceed_e${id} = ace.edit("editor_e${id}");
-      aceed_e${id}.setTheme("ace/theme/solarized_light");
-      aceed_e${id}.session.setMode("ace/mode/html");
-      aceed_e${id}.setOptions(acegeneralconfig_e${id});
-      
-      </script>
 
     `;
   document.getElementById("res" + id).innerHTML = code;
