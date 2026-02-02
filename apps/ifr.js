5312df61e37a28c71871d26c51e3841cd64888d2
Guilherme Ranoya <ranoya@gmail.com>
2026-02-02 18:20:56 -0300

    difícil alterar as propriedades na mão

--- apps/ifr.js
+++ apps/ifr.js
@@ -36,10 +36,7 @@ ifr = function (t, u, menu, c) {
     document.getElementById(barramenu).style.display = "none";
     document.getElementById("res" + id).style.height =
       "calc(100% - 3rem + 8px)";
-
-    document.getElementById("res" + id).style.paddingLeft = "0 !important";
-    document.getElementById("res" + id).style.paddingRight = "0 !important";
-    document.getElementById("res" + id).style.width = "100% !important";
+    document.getElementById("res" + id).classList.add("ifrnomenu");
   }
 
   setTimeout(function () {
